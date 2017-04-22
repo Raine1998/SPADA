4fec47df45cace6684e95bbaceb68c906661c294
rameshangi <angithar@gmail.com>
2017-04-22 15:39:43 +1200

    Update Engine.java
    updated

--- Engine.java
+++ Engine.java
@@ -9,17 +9,24 @@ public class Engine {
     int score = 0;
     double speed = 10;   // Movement speed of Obstacles and pick ups
     int mood = 5;
-    boolean up = false;    // Boolean Value to determine movement direction
+    
+    int highScore = 0;
 
-    Character bird = new Character( 5, 50, 50);
-    Pickup pUp = new Pickup(randInt(10));
-    Obstacle cloudA = new Obstacle(5);
-    Obstacle cloudB = new Obstacle(5);
+    boolean up = false;    // Boolean Value to determine movement direction
+    int birdState = 1;
+
+    Character bird = new Character(50, 50);
+    Pickup pUp = new Pickup();
+    Obstacle cloudA = new Obstacle(mood);
+    Obstacle cloudB = new Obstacle(mood);
+    Sound music= new Sound("BGMusic.wav");
 
     public Engine()
     {
-        UI.setDivider(0.0);
         UI.initialise();
+        UI.setWindowSize(1200, 700);
+        UI.setDivider(0.0);
+
         UI.setKeyListener(this::direction);   
         UI.setFontSize(20);
         UI.setDivider(0.0);
@@ -28,8 +35,8 @@ public class Engine {
     }
 
     public void direction(String dir){
-        if(dir.equals("w")){up = true;}
-        else if(dir.equals("s")){up = false;}
+        if(dir.equals("Up")){up = true;}
+        else if(dir.equals("Down")){up = false;}
     }
 
     public void playGame()
@@ -37,20 +44,30 @@ public class Engine {
         reset();
 
         while( state.equals("play")){
+            music.loop();
+            
             processCollisions();
-            updatePositions();
             drawAssets();
+            updatePositions();
 
             score += speed;
+
+            if ( mood < 1 || mood > 10){ state = "game over";}
         }
 
         if ( state.equals("game over"))
         {
+            music.stop();
             drawBG(5, 0, 0);
             UI.setFontSize(40);
-            UI.drawString("GAME OVER", 300, 100 );
+            UI.drawString("GAME OVER", 350, 100 );
             UI.setFontSize(20);
-            UI.drawString("Score: " + score, 300, 200);
+            
+            if( score > highScore)
+                { highScore = score;}
+            UI.drawString("High Score: " + highScore, 400, 200);    
+            UI.drawString("Your Score: " + score, 400, 400);
+
         }
     }
 
@@ -58,25 +75,32 @@ public class Engine {
     {
         String fname = null;
         fname = "Scene" + "/" +  mood + ".png"; 
-        UI.drawImage(fname, 0, 0, 18000, 500);
+        UI.drawImage(fname, 0, 0, 1200, 500);
     }
 
     public void updatePositions(){
         pUp.setX( pUp.getX() - speed * 2);
+        cloudA.setMood(mood);
         cloudA.setX( cloudA.getX() - speed * 2);
+        cloudB.setMood(mood);
         cloudB.setX( cloudB.getX() - speed * 2);
         if(up && bird.getY() > 0){bird.setY(bird.getY() - 15) ;}
-        else if (bird.getY() < 452){bird.setY(bird.getY() + 15) ;}
+        else if (bird.getY() < 490 - bird.getHeight()){bird.setY(bird.getY() + 15) ;}
 
     }
 
     public void drawAssets(){
+
         drawBG(mood, 0, 0);
         UI.drawString("Score: " + score, 700, 50);
+        UI.drawString("Mood: " + mood, 700, 150);
         if (pUp.getX() < (0 - pUp.getWidth())){ pUp.reDraw();} else{pUp.draw();}
         if (cloudA.getX() < (0 - cloudA.getWidth())){cloudA.reDraw();}else{cloudA.draw();}
         if (cloudB.getX() < (0 - cloudB.getWidth())){cloudB.reDraw();}else{cloudB.draw();}
-        bird.draw();
+        if(birdState == 0){bird.drawA(); birdState = 1;}
+        else if(birdState == 1){bird.drawB(); birdState = 2;}
+        else if(birdState == 2){bird.drawC(); birdState = 3;}
+        else if(birdState == 3){bird.drawC(); birdState = 0;}
     }
 
     public void processCollisions(){
@@ -86,38 +110,48 @@ public class Engine {
         {
             UI.sleep(50);
             drawBG(5, 0, 0);
+
+            if(pUp.getPos()){
+                mood++;
+                speed += 2.5;
+            }
+            else {
+                mood--;
+                speed--;
+
+            }
+
             pUp.reDraw();
-            setGlobalSpeed( pUp.getMood());   
         }
 
-        if ( ( cloudA.getX()  <= bird.getX() + bird.getWidth()) 
-        && ( cloudA.getY() + cloudA.getHeight() > bird.getY() + bird.getHeight()  
-            &&  cloudA.getY() < bird.getY()))
+        if ( ( cloudA.getX()  <= bird.getX() + bird.getWidth() && cloudA.getX() + cloudA.getWidth() - 25 >= bird.getX()) 
+        && ( cloudA.getY() + cloudA.getHeight() >= bird.getY() + bird.getHeight()  
+            &&  cloudA.getY() <= bird.getY()))
         {
             UI.sleep(50);
             drawBG(5, 0, 0);
             state = "game over";
         }
 
-        if ( ( cloudB.getX()  <= bird.getX() + bird.getWidth()) 
-        && ( cloudB.getY() + cloudB.getHeight() > bird.getY() + bird.getHeight() 
-            &&  cloudB.getY() < bird.getY()  ))
+        if ( ( cloudB.getX()  <= bird.getX() + bird.getWidth() && cloudB.getX() + cloudB.getWidth()- 25 >= bird.getX()) 
+        && ( cloudB.getY() + cloudB.getHeight() >= bird.getY() + bird.getHeight() 
+            &&  cloudB.getY() <= bird.getY()  ))
         {
             UI.sleep(50);
             drawBG(5, 0, 0);
             state = "game over";
         }
 
-        if ( ( pUp.getX()  >= cloudA.getX() && pUp.getX()  <= cloudA.getX() + cloudA.getWidth()) 
-        && ( pUp.getY()+ pUp.getHeight() > cloudA.getY() 
-            &&  pUp.getY() < cloudA.getY() + cloudA.getHeight()))
+        if ( ( pUp.getX() + 50  >= cloudA.getX() && pUp.getX()  <= cloudA.getX() + cloudA.getWidth()) 
+        && ( pUp.getY()+ pUp.getHeight() > cloudA.getY() - 50
+            &&  pUp.getY() + 50 < cloudA.getY() + cloudA.getHeight()))
         {
             pUp.reDraw();
         }
 
-        if ( ( pUp.getX()  >= cloudB.getX() && pUp.getX()  <= cloudB.getX() + cloudB.getWidth()) 
-        && ( pUp.getY()+ pUp.getHeight() > cloudB.getY() 
-            &&  pUp.getY() < cloudB.getY() + cloudB.getHeight()))
+        if ( ( pUp.getX()+ 50  >= cloudB.getX() &&  pUp.getX()  <= cloudB.getX() + cloudB.getWidth()) 
+        && ( pUp.getY()+ pUp.getHeight() > cloudB.getY() - 50 
+            &&  pUp.getY() + 50 < cloudB.getY() + cloudB.getHeight()))
         {
             pUp.reDraw();
         }
@@ -130,21 +164,13 @@ public class Engine {
         }
     }
 
-    public void setGlobalSpeed(int m){
-        speed += m / 2;
-        //bird.setMood(m);
-        pUp.setMood(m);
-        //cloud.setMood(m);
-
-    }
 
     public void reset(){
-
         state = "play"; // game state "play", "game over" or "menu" (not implemented yet);
         score = 0;
         speed = 10;   // Movement speed of Obstacles and pick ups
         mood = 5;
-        Character bird = new Character( 5, 50, 50);
+        Character bird = new Character( 50, 50);
         pUp.reDraw();
         cloudA.reDraw();
         cloudB.reDraw();
